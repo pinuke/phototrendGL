de3fb4e8fe9ec5208e679a5c944473da85420b77
Nicholas Jackson <smartguy1196@gmail.com>
2017-09-13 04:10:26 -0700

    Bug-testing Logs and Log Patch
    Script does not run as expected
    - adding logs to show how much and what got executed
    
    Log Patch
    - Move line breaking into log function
    - Change some conventions

--- docs/tests/20170912/graph.js
+++ docs/tests/20170912/graph.js
@@ -1,21 +1,24 @@
 function log( input ){
-  document.getElementById( "log" ).innerHTML += input;
+  document.getElementById( "log" ).innerHTML += input + "<br/>";
 }
 
-function loadGraph( image ){
+function loadGraph( name, image ){
   var two_d = document.createElement( "canvas" );
   var context = two_d.getContext( '2d' );
   context.drawImage( image, 0, 0 );
   var imageData = context.getImageData( 0, 0, image.width - 1, image.height - 1);
-  log( "imageData:<br/>" + JSON.stringify( imageData, null, '\t' ) + "<br/>" );
+  log( "imageData:<br/>" + JSON.stringify( imageData, null, '\t' ) );
 }
 
 function renderImages( uploads )
 {
+  log( "Checking files..." );
   if( uploads && uploads[0] )
   {
+    log( "Files detected: " + uploads.length + " files" );
     for( var i = 0; i < uploads.length; i++ )
     {
+      log( uploads[i].name + ": Checking file extension..." );
       if( (/\.(png|jpeg|jpg)$/i).test(uploads[i].name) )
       {
         var img = new Image;
@@ -24,26 +27,29 @@ function renderImages( uploads )
           window.URL.revokeObjectURL( image.src );
         }
         img.src = window.URL.createObjectURL(uploads[i]);
-        log( "filename: " + uploads[i].name + "<br/>" );
-        loadGraph( image )
+        log( "Graphable File: " + uploads[i].name );
+        log( uploads[i].name + ": Graphing..." );
+        loadGraph( uploads[i].name, image );
+        log( uploads[i].name + ": Image graphed" );
       }
       else if( (/\.(gif|apng)$/i).test(uploads[i].name) )
       {
-        log( uploads[i].name +" - Image extension not supported at this time<br/>" );
+        log( uploads[i].name +": Image extension not supported at this time!" );
       }
       else
       {
-        log( uploads[i].name +" - Extension not supported<br/>" );
+        log( uploads[i].name +": Extension not supported!" );
       }
     }
   }
   else
   {
-    log( "No files provided during upload to renderImage()<br/>" );
+    log( "No files provided during upload to renderImage()" );
   }
 }
 
 document.getElementById( "upload" ).onChange = function()
 {
+  log( "Input detected!" );
   renderImages( this.files );
 }
