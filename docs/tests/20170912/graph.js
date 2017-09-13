5df8ad79b6f4d363d0cb1cbe05446ebd0beef002
Nicholas Jackson <smartguy1196@gmail.com>
2017-09-13 03:07:47 -0700

    Features Pushed - WIP
    - Added a renderImage function for handling the uploads
    - Replaced original function with a loadGraph function - WIP
    - Started work on a logging function - WIP

--- docs/tests/20170912/graph.js
+++ docs/tests/20170912/graph.js
@@ -1,12 +1,46 @@
-funtion getVertices( image ){
-  var virtual = document.createElement( "canvas" );
-  var context = virtual.getContext('2d');
-  context.drawImage(image, x, y);
-  return context.getImageData(x1, y1, x2, y2)
+function log( input ){
+
 }
 
-function displayPreview( uploads ) {
-    var reader = new FileReader();
-    reader.onload = onFileLoad;
-    reader.readAsDataURL(files[0]);
+funtion loadGraph( image ){
+  var two_d = document.createElement( "canvas" );
+  var context = two_d.getContext( '2d' );
+  context.drawImage( image, 0, 0 );
+  var imageData = context.getImageData( 0, 0, image.width - 1, image.height - 1);
+  log( "imageData:<br/>" + JSON.stringify( imageData, null, '\t' ) + "<br/>" );
 }
+
+function renderImages( uploads )
+{
+  var errors="";
+  
+  if( uploads && uploads[0] )
+  {
+    for( var i = 0; i < uploads.length; i++ )
+    {
+      if( (/\.(png|jpeg|jpg)$/i).test(uploads[i].name) )
+      {
+        var img = new Image;
+        img.onload = function()
+        {
+          window.URL.revokeObjectURL(image.src);
+        }
+        img.src = window.URL.createObjectURL(uploads[i]);
+        loadGraph( image )
+      }
+      else if( (/\.(gif|apng)$/i).test(uploads[i].name) )
+      {
+        errors += uploads[i].name +" - Image extension not supported at this time<br/>";
+      }
+      else
+      {
+        errors += uploads[i].name +" - Extension not supported<br/>";
+      }
+    }
+  }
+  else
+  {
+    errors += "No files provided during upload to renderImage()<br/>";
+  }
+}
+
