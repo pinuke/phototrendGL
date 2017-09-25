ce53765dd7521beca8fc66d45b1a975dcbed2929
Nicholas Jackson <smartguy1196@gmail.com>
2017-09-25 03:09:34 -0800

    Add Colorscales for Each Graph

--- docs/tests/20170924/renderGraph.js
+++ docs/tests/20170924/renderGraph.js
@@ -1,18 +1,47 @@
 function renderGraph( plot ){
-  // initial data..
   z1 = plot.vertices.red
-  // generating data for other traces..
   z2 = plot.vertices.green
-
   z3 = plot.vertices.blue
-  
   z4 = plot.vertices.alpha
 
   // creating data objects..
-  var data_z1 = {z: z1, type: 'surface'};
-  //var data_z2 = {z: z2, showscale: false, opacity:0.9, type: 'surface'};
-  //var data_z3 = {z: z3, showscale: false, opacity:0.9, type: 'surface'};
+  var data_z1 = {
+    z: z1,
+    showscale: false,
+    type: 'surface',
+    colorscale: [ 
+      [0, 'rgb(0,0,0)'], 
+      [1, 'rgb(255,0,0)']
+    ]
+  };
+  var data_z2 = {
+    z: z2,
+    showscale: false,
+    type: 'surface',
+    colorscale: [ 
+      [0, 'rgb(0,0,0)'], 
+      [1, 'rgb(0,255,0)']
+    ]
+  };
+  var data_z3 =  {
+    z: z3,
+    showscale: false,
+    type: 'surface',
+    colorscale: [ 
+      [0, 'rgb(0,0,0)'], 
+      [1, 'rgb(0,0,255)']
+    ]
+  };
+  var data_z4 =  {
+    z: z4,
+    showscale: false,
+    type: 'surface',
+    colorscale: [ 
+      [0, 'rgb(0,0,0)'], 
+      [1, 'rgb(230,230,230)']
+    ]
+  };
 
   // Plotting the surfaces..
-  Plotly.newPlot('myDiv', [data_z1]);
+  Plotly.newPlot('myDiv', [data_z4]);
 }
