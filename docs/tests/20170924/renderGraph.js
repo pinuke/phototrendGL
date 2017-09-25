3d5e43dec67cb3974e29a8858e8e67857879b3e4
Nicholas Jackson <smartguy1196@gmail.com>
2017-09-25 02:07:06 -0800

    Update renderGraph.js

--- docs/tests/20170924/renderGraph.js
+++ docs/tests/20170924/renderGraph.js
@@ -1,3 +1,18 @@
 function renderGraph( plot ){
+  // initial data..
+  z1 = plot.vertices.red
+  // generating data for other traces..
+  z2 = plot.vertices.green
 
+  z3 = plot.vertices.blue
+  
+  z4 = plot.vertices.alpha
+
+  // creating data objects..
+  var data_z1 = {z: z1, type: 'surface'};
+  //var data_z2 = {z: z2, showscale: false, opacity:0.9, type: 'surface'};
+  //var data_z3 = {z: z3, showscale: false, opacity:0.9, type: 'surface'};
+
+  // Plotting the surfaces..
+  Plotly.newPlot('myDiv', [data_z1]);
 }
