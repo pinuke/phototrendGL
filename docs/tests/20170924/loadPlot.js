b8b70a22a7057dddbd6d62c2f8c8ae9c9f78e863
Nicholas Jackson <smartguy1196@gmail.com>
2017-09-25 00:32:10 -0800

    Prepare file for updated file structure
    - extract all other calls and functions outside of loadPlot
    - rename to loadPlot.js

--- docs/tests/20170924/loadPlot.js
+++ docs/tests/20170924/loadPlot.js
@@ -1,7 +1,3 @@
-function log( input ){
-  document.getElementById( "log" ).innerHTML += input + "<br/>";
-}
-
 function loadPlot( name, image ){
   var two_d = document.createElement( "canvas" );
   two_d.width = image.width;
@@ -133,43 +129,3 @@ function loadPlot( name, image ){
   return plot;
   
 }
-
-function renderImages( uploads )
-{
-  if( uploads && uploads[0] )
-  {
-//    log( "Files detected: " + uploads.length + " files" );
-    for( var i = 0; i < uploads.length; i++ )
-    {
-//      log( uploads[i].name + ": Checking file extension..." );
-      if( (/\.(png|jpeg|jpg)$/i).test(uploads[i].name) )
-      {
-//        log( uploads[i].name + ": File is graphable! Graphing it..." );
-        var img = new Image;
-        (function (val) {
-          img.addEventListener( "load", function (){
-            loadGraph( val, this );
-            window.URL.revokeObjectURL( this.src );
-          })
-        })(uploads[i].name);
-        img.src = window.URL.createObjectURL(uploads[i]);
-      }
-      else if( (/\.(gif|apng)$/i).test(uploads[i].name) )
-      {
-        log( uploads[i].name +": Image extension not supported at this time!" );
-      }
-      else
-      {
-        log( uploads[i].name +": Extension not supported!" );
-      }
-    }
-  }
-  else
-  {
-    log( "No files provided during upload to renderImage()" );
-  }
-}
-
-document.getElementById( "upload" ).addEventListener("change", function(){
-  renderImages( this.files );
-});
