07789e55bb1250b85c0a9a5ad01b406d2bf50ddf
smartguy1196 <smartguy1196@gmail.com>
2017-10-01 10:31:58 -0700

    Add readability, and Add Sides (Triangles)

--- docs/tests/20170925/extractPlot.js
+++ docs/tests/20170925/extractPlot.js
@@ -36,12 +36,23 @@ function extractImages( uploads, callback )
             
             var vertices = plot.vertices;
 			var heights = [];
+			var coords = {
+				"TL" : [],
+				"TR" : [],
+				"BR" : [],
+				"BL" : [],
+				"ra" : [],
+				"rb" : [],
+				"ha" : [],
+				"hb" : []
+			}
+			var edge;
 			var channels = [ "red", "blue", "green", "alpha" ]
   
-            function index_of_pixel( x, y ){
+            function index_of_pixel( coord_array ){
               var coeff = imageData.width * 4 //data per row of pixels should be the image width multiplied by 4 for R, G, B, + A values
-              var ret = coeff * y //index of first pixel in y rows of pixels
-              ret += x * 4 //index of pixel
+              var ret = coeff * coord_array[1] //index of first pixel in y rows of pixels
+              ret += coord_array[0] * 4 //index of pixel
               return ret
             }
             for( var i = 0; i < imageData.height - 1; i++)
@@ -52,30 +63,100 @@ function extractImages( uploads, callback )
 					
 					channels.forEach( function( name, ind ){
 						
-						height[0] = imageData.data[ index_of_pixel( c, i ) + ind ]
-						height[1] = imageData.data[ index_of_pixel( c + 1, i ) + ind ]
-						height[2] = imageData.data[ index_of_pixel( c + 1, i + 1 ) + ind ]
-						height[3] = imageData.data[ index_of_pixel( c, i + 1 ) + ind ]
+						coords.TL = [ c, i ];
+						coords.TR = [ c + 1, i ];
+						coords.BR = [ c + 1, i + 1 ];
+						coords.BL = [ c, i + 1 ];
 						
-						vertices[ name ].concat([
+						height[0] = imageData.data[ index_of_pixel( coords.TL ) + ind ]
+						height[1] = imageData.data[ index_of_pixel( coords.TR ) + ind ]
+						height[2] = imageData.data[ index_of_pixel( coords.BR ) + ind ]
+						height[3] = imageData.data[ index_of_pixel( coords.BL ) + ind ]
 						
-							c, i, height[0],
-							c + 1, i, height[1],
-							c + 0.5, i + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
+						vertices[ name ] = vertices[ name ].concat([
+						
+							coords.TL[0], coords.TL[1], height[0],
+							coords.TR[0], coords.TR[1], height[1],
+							coords.TL[0] + 0.5, coords.TL[1] + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
 							
-							c + 1, i, height[1],
-							c + 1, i + 1, height[2],
-							c + 0.5, i + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
+							coords.TR[0], coords.TR[1], height[1],
+							coords.BR[0], coords.BR[1], height[2],
+							coords.TL[0] + 0.5, coords.TL[1] + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
 							
-							c + 1, i + 1, height[2],
-							c, i + 1, height[3],
-							c + 0.5, i + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
+							coords.BR[0], coords.BR[1], height[2],
+							coords.BL[0], coords.BL[1], height[3],
+							coords.TL[0] + 0.5, coords.TL[1] + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
 							
-							c, i + 1, height[3],
-							c, i, height[0],
-							c + 0.5, i + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
+							coords.BL[0], coords.BL[1], height[3],
+							coords.TL[0], coords.TL[1], height[4],
+							coords.TL[0] + 0.5, coords.TL[1] + 0.5, ( height[0] + height[1] + height[2] + height[3] ) / 4,
 						
 						])
+						
+						edge = true;
+						
+						if( i === 0 )
+						{
+							
+							coords.ra = coords.TL
+							coords.ha = height[0]
+							coords.rb = coords.TR
+							coords.hb = height[1]
+							
+						}
+						else
+						
+						if( c === imageData.width - 2 )
+						{
+							
+							coords.ra = coords.TR
+							coords.ha = height[1]
+							coords.rb = coords.BR
+							coords.hb = height[2]
+							
+						}
+						else
+						
+						if( i === imageData.height - 2 )
+						{
+							
+							coords.ra = coords.BR
+							coords.ha = height[2]
+							coords.rb = coords.BL
+							coords.hb = height[3]
+							
+						}
+						else
+						
+						if( c === 0 )
+						{
+							
+							coords.ra = coords.BL
+							coords.ha = height[3]
+							coords.rb = coords.TL
+							coords.hb = height[0]
+							
+						}
+						else
+							edge = false;
+						
+						if( edge ){
+							
+							vertices[ name ] = vertices[ name ].concat(
+							
+								coord.ra.concat( coord.ha ),
+								coord.rb.concat( coord.hb ),
+								
+								coord.ra.concat( 0 ),
+								
+								coord.ra.concat( 0 ),
+								coord.rb.concat( 0 ),
+								
+								coord.rb.concat( coord.hb )
+								
+							)
+							
+						}
 						
 					})
 					
